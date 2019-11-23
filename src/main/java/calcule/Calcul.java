e8acb0c673e336f325b2e59fbf0822ae24e3048f
modoue <modou2015diop@gmail.com>
2019-11-23 16:39:22 +0100

    firste

--- src/main/java/calcule/Calcul.java
+++ src/main/java/calcule/Calcul.java
@@ -1,5 +1,17 @@
 package calcule;
 
 public class Calcul {
+	public Calcul()
+	{
+		
+	}
+	public int addition(int a,int b)
+	{
+		return a+b;
+	}
+public int multiplication(int a,int b)
+{
+	return a*b;
+}
 
 }
--- src/test/java/calcule/Test.java
+++ src/test/java/calcule/Test.java
@@ -5,14 +5,16 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 
 public class Test {
-
+private Calcul cacul;
 	@Before
 	public void setUp() throws Exception {
+		cacul=new Calcul();
 	}
 
 	@org.junit.Test
 	public void test() {
-		fail("Not yet implemented");
+		assertEquals(10, cacul.addition(10, 0));
+		
 	}
 
 }
